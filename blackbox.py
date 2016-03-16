f2a024c2e40729164a9c81ff1dd888c53c7c1bd6
Suvigya Tripathi <suvigya91@gmail.com>
2016-03-16 16:43:09 -0400

    Refactoring module test. Making tool independent of kernel.

--- blackbox.py
+++ blackbox.py
@@ -17,6 +17,7 @@ import argparse
 import imp
 import glob
 import shutil
+from colorama import *
 
 from shutil import copyfile
 from os.path import expanduser
@@ -27,11 +28,8 @@ class Standalone():
     def __init__(self,
                  name,
                  resource,
-                 inp_files,
-                 grid = 5,
-                 dims = 3,
-                 num_CUs = 4,
-                 atom = 'protein'
+                 arguments,
+                 modules = None
                  ):
         self.pre_exec = []
         self.wdir = None
@@ -45,79 +43,91 @@ class Standalone():
         self.resource = resource
         self.job_id = None
         self.exitcode = None
-        self.grid = grid
-        self.dims = dims
-        self.num_CUs = num_CUs
-        self.atom = atom
         self.home = None
-        self.module_stage_cmd = None
-        self.inp_file = inp_files
-        self.module_test_file = None
+        self.inp_file = arguments
         self.output_file = None
-        self.param = None
-        self.only_args = None
-
-##    def copyModuleTestFile(self):
-##        #-------------------------------------------------------------------------------------------------
-##        #Create directories
-##        p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'mkdir -p Standalone/Output'])
-##        stdout, stderr = p.communicate()
-##
-##        p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'mkdir -p Standalone/input'])
-##        stdout, stderr = p.communicate()
-##
-##        #-------------------------------------------------------------------------------------------------
-##        #transfer in-built input files for module check stage
-##
-##        p = subprocess.Popen(['scp', '-r', '/home/suvigya/Blackbox/' , '%s@stampede.tacc.utexas.edu:/%s'%(jd.uname,jd.wdir)],
-##                             stdin=subprocess.PIPE,
-##                             stdout=subprocess.PIPE,
-##                             stderr=subprocess.PIPE)
-##        stdout, stderr = p.communicate()
+        self.user_modules = modules
 
     ###################################################################################################
-    def copyFilesToRemote(self, inp_files,stage):
-        #-------------------------------------------------------------------------------------------------
-        #Create directories
-        p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'mkdir -p Standalone/Output'])
-        stdout, stderr = p.communicate()
-
-        p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'mkdir -p Standalone/user_files'])
-        stdout, stderr = p.communicate()
-
+    def copyFilesToRemote(self, inp_files, stage, module):
         #-------------------------------------------------------------------------------------------------
         #transfer in-built input files for module check stage
         if(stage == 0):
-            p = subprocess.Popen(['scp', '-r', '%s/input/'%LOCAL_HOME , '%s@stampede.tacc.utexas.edu:/%s/Standalone/'%(self.uname,self.wdir)],
-                                 stdin=subprocess.PIPE,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
+            target = open('module_error.log','w')
+            test_mod = ""
+            #if self.user_modules is None:
+            for mod in module:
+                test_mod += "%s\n"%mod
+                #print "Testing ",test_mod
+            p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'%s'%test_mod],
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
             stdout, stderr = p.communicate()
-        
+            target.write(stderr)
+            target.write('\n\n')
+            for line in stderr.split("\n"):
+                if (("error" in line) or ("Error" in line)):
+                    print(Fore.RED+"Module loading error, Check module_error file and add modules explicitly!!"+Fore.RESET)
+                    sys.exit(1)
+
+            print(Fore.GREEN+"Modules loaded correctly"+Fore.RESET)
+            target.close()
+
         #-------------------------------------------------------------------------------------------------
         #Copy user input files for checking at stage 2
         if(stage == 1):
-            for f in inp_files:
-                #print f
-                argument,fname = f.split('=')
-                print fname
-                p = subprocess.Popen(['scp', '-r','%s'%fname , '%s@stampede.tacc.utexas.edu:/%s/Standalone/user_files'%(self.uname,self.wdir)])
-##                                 stdin=subprocess.PIPE,
-##                                 stdout=subprocess.PIPE,
-##                                 stderr=subprocess.PIPE)
-                stdout, stderr = p.communicate()
-                if stderr is "":
-                    print fname, 'transferred'
+            #-------------------------------------------------------------------------------------------------
+            #Create directories
+            p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'mkdir -p Standalone/Output'])
+            stdout, stderr = p.communicate()
+    
+            p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'mkdir -p Standalone/user_files'])
+            stdout, stderr = p.communicate()
 
-        p = subprocess.Popen(['scp', '%s/slurm_script.slurm'%LOCAL_HOME , '%s@stampede.tacc.utexas.edu:/%s/Standalone/'%(self.uname,self.wdir)],
+            for f in inp_files:
+                #Transfer files without argument
+                if ('=' not in f and '/' in f):
+                    p = subprocess.Popen(['scp','%s'%f , '%s@stampede.tacc.utexas.edu:/%s/Standalone/user_files'%(self.uname,self.wdir)],
+                                            stdin=subprocess.PIPE,
+                                            stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE)
+                    stdout, stderr = p.communicate()
+                    if stderr is "":
+                        print(f+' transferred')
+                
+                elif '=' in f:
+                    #transfer file with argument
+                    argument,fname = f.split('=')
+                    if not fname.isdigit():
+                        p = subprocess.Popen(['scp','%s'%fname , '%s@stampede.tacc.utexas.edu:/%s/Standalone/user_files'%(self.uname,self.wdir)],
+                                     stdin=subprocess.PIPE,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+                        stdout, stderr = p.communicate()
+                        if stderr is "":
+                            print fname, 'transferred'
+
+                        if "*" in fname:
+                            for files in glob.glob(fname):
+                                #print files
+                                p = subprocess.Popen(['scp','%s'%files , '%s@stampede.tacc.utexas.edu:/%s/Standalone/user_files'%(self.uname,self.wdir)],
+                                             stdin=subprocess.PIPE,
+                                             stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE)
+                                stdout, stderr = p.communicate()
+                                if stderr is "":
+                                    print(files+' transferred')
+
+            p = subprocess.Popen(['scp', '%s/slurm_script.slurm'%LOCAL_HOME , '%s@stampede.tacc.utexas.edu:/%s/Standalone/'%(self.uname,self.wdir)],
                             stdin=subprocess.PIPE,
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate()
+            stdout, stderr = p.communicate()
 
 
     ###################################################################################################
-    def generateSlurm(self,stage, inp_files):
+    def generateSlurm(self,inp_files):
         self.loadConfig()
 
         pwd = "%s/Standalone"%self.wdir
@@ -147,51 +157,29 @@ class Standalone():
         for mod in self.pre_exec:
             slurm_script += "%s\n" % mod
 
-        if(stage == 0):
-            print "Module test"
-            slurm_script += "\n\nibrun %s "%self.exe
+        #print "Execution test"
+        slurm_script += "\n\nibrun %s "%self.exe
 
-            if self.only_args is not None:
-                for a in self.only_args:
-                    slurm_script += "%s "%(a)                 
+        for f in inp_files:
+            if (('=' not in f) and ('/' not in f)):
+                slurm_script += "%s "%f
 
-            if self.param is not None:
-                for p in self.param:
-                    argument, pname = p.split('=')
-                    slurm_script += "%s %s "%(argument,pname)                 
-            
-            for f in self.module_test_file:
-                argument, fname = f.split('=')
-                slurm_script += "%s %s/input/%s "%(argument,pwd,os.path.basename(fname))
-                
-            if self.output_file is not None:
-                for f in self.output_file:
-                    argument, fname = f.split('=')
-                    slurm_script += "%s %s/Output/%s "%(argument,pwd,os.path.basename(fname))
-            slurm_script += "\n"
+            elif (('=' not in f) and ('/' in f)):
+                slurm_script += "%s/user_files/%s "%(pwd,os.path.basename(f))
+            else:
+                argument, fname = f.split('=') 
+                if ((fname.isdigit()) or ('/' not in fname)):
+                    slurm_script += "%s %s "%(argument,fname)
+                    
+                elif '=' in f:
+                    slurm_script += "%s %s/user_files/%s "%(argument,pwd,os.path.basename(fname))
 
-        elif(stage == 1):
-            slurm_script += "\n\nibrun %s "%self.exe
+        if self.output_file is not None:
+            for f in self.output_file:
+                argument, fname = f.split('=') 
+                slurm_script += "%s %s/Output/%s "%(argument,pwd,os.path.basename(fname))
 
-            if self.only_args is not None:
-                for a in self.only_args:
-                    slurm_script += "%s "%(a)                 
-
-            if self.param is not None:
-                for p in self.param:
-                    argument, pname = p.split('=')
-                    slurm_script += "%s %s "%(argument,pname)                 
-
-            for f in inp_files:
-                argument, fname = f.split('=')
-                slurm_script += "%s %s/user_files/%s "%(argument,pwd,os.path.basename(fname))
-
-            if self.output_file is not None:
-                for f in self.output_file:
-                    argument, fname = f.split('=')
-                    slurm_script += "%s %s/Output/%s "%(argument,pwd,os.path.basename(fname))
-
-            slurm_script += "\n"
+        slurm_script += "\n"
             
         return slurm_script
 
@@ -223,10 +211,9 @@ class Standalone():
             self.exe = k_cfg["executable"]
             self.pre_exec = Kconfig.stampede_module
             self.environment = Kconfig.stampede_environment
-            self.module_test_file = Kconfig.module_test_file
+            #self.module_test_file = Kconfig.module_test_file
             self.output_file = Kconfig.output_file
-            self.param = Kconfig.param
-            self.only_args = Kconfig.only_args
+            #self.param = Kconfig.param
             #self.module_stage_cmd = Kconfig.stage_module_load
         except Exception:
             print 'error'
@@ -236,7 +223,7 @@ class Standalone():
     #---------------------------------------------------------------------------------------------------------
     def job_submit(self):
         #Submit job
-        print "Submitting slurm job"
+        print(Fore.GREEN+"Submitting slurm job"+Fore.RESET)
         p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'sbatch %s/Standalone/slurm_script.slurm'%self.wdir],
                              stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
@@ -249,9 +236,9 @@ class Standalone():
                 self.job_id = "%s" %int(line.split()[-1:][0])
                 #print line
         if self.job_id!= None:
-            print "Submission successful: Job id = ",self.job_id
+            print(Fore.GREEN+"Submission successful: Job id = "+self.job_id+Fore.RESET)
         else:
-            print "Submission failure"
+            print(Fore.RED+"Submission failure"+Fore.RESET)
             sys.exit(1)
 
 
@@ -260,7 +247,7 @@ class Standalone():
         #Check for job completion
         command = ['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'squeue --job', self.job_id]
         state = None
-        print "Waiting for code to execute..."
+        print(Fore.GREEN+"Waiting for code to execute..."+Fore.RESET)
         while(state != "CG"):
             p = subprocess.Popen(command,
                                  stdin=subprocess.PIPE,
@@ -273,14 +260,14 @@ class Standalone():
                     state = "CG"
                     break
             time.sleep(3)
-        print "Execution complete..."
+        print(Fore.GREEN+"Execution complete..."+Fore.RESET)
         print "--------> %s <---------"%state
 
 
     #---------------------------------------------------------------------------------------------------------------
     def error_check(self):
         if(self.kernel_name == "amber" or self.kernel_name == "coco" or self.kernel_name == "lsdmap"):
-            print "checking error in %s"%self.kernel_name
+            print(Fore.GREEN+"checking error in "+self.kernel_name+Fore.RESET)
             p = subprocess.Popen(['ssh','%s@stampede.tacc.utexas.edu'%self.uname,'cat %s/Standalone/Output/STDERR'%self.wdir],
                                  stdin=subprocess.PIPE,
                                  stdout=subprocess.PIPE,
@@ -310,6 +297,10 @@ class Standalone():
     #------------------------------------------------------------------------------------------------------------------
     def cleanup(self):
         #Transfer files from remote to local machine
+        files = glob.glob('%s/Output/*'%LOCAL_HOME)
+        for f in files:
+            os.remove(f)
+
         print "Transfer output to local machine"
         p = subprocess.Popen(['scp', '-r', '%s@stampede.tacc.utexas.edu:/%s/Standalone/Output/'%(self.uname,self.wdir), '%s/'%LOCAL_HOME ],
                              stdin=subprocess.PIPE,
@@ -337,44 +328,43 @@ class Standalone():
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         stdout, stderr = p.communicate()
-        print "Removed all the files from remote machine"
+        print(Fore.GREEN+"Removed all the files from remote machine"+Fore.RESET)
 
 
     #--------------------------------------------------------------------------------------------------------
     def run(self):
         #-------------------------------------------------------------------------------------------------------
-        slurm_script = self.generateSlurm(0,self.inp_file)
-       
-        target = open('slurm_script.slurm','w')
-        target.write(slurm_script)
-        target.close()
-
-        self.copyFilesToRemote(self.inp_file,0)
-        self.job_submit()
-        self.job_check()
-        self.error_check()
-#        self.cleanup()
-        if(self.exitcode == 1):
-            print "Modules incorrectly loaded, check STDERR file. Also update configs/%s.wcfg"%self.kernel_name
-            sys.exit(1)
+        #Module test
+        print(Fore.GREEN+"******************************************"+Fore.RESET)
+        print(Fore.GREEN+"*            Module Test: "+self.kernel_name+"        *"+Fore.RESET)
+        print(Fore.GREEN+"******************************************"+Fore.RESET)
+        slurm_script = self.generateSlurm(self.inp_file)
+#        print self.user_modules
+        if self.user_modules is None:
+            print(Fore.YELLOW+"No user modules found! Using default modules"+Fore.RESET)
+            self.copyFilesToRemote(self.inp_file,0,self.pre_exec)
         else:
-            print "All the modules are loaded correctly."
+            print(Fore.YELLOW+"Using user modules")
+            self.copyFilesToRemote(self.inp_file,0,self.user_modules)
 
-        #---------------------------------------------------------------------------------------------------------
-        slurm_script = self.generateSlurm(1,self.inp_file)
+##        #---------------------------------------------------------------------------------------------------------
+        print(Fore.GREEN+"\n******************************************"+Fore.RESET)
+        print(Fore.GREEN+"*            Execution Test: "+self.kernel_name+"     *"+Fore.RESET)
+        print(Fore.GREEN+"******************************************"+Fore.RESET)
+        slurm_script = self.generateSlurm(self.inp_file)
        
         target = open('slurm_script.slurm','w')
         target.write(slurm_script)
         target.close()
 
-        self.copyFilesToRemote(self.inp_file,1)
+        self.copyFilesToRemote(self.inp_file,1,"")
         self.job_submit()
         self.job_check()
         self.error_check()
         self.cleanup()
         if(self.exitcode == 1):
-            print "Execution failed, Check STDERR file"
+            print(Fore.RED+"Execution failed, Check STDERR file"+Fore.RESET)
             sys.exit(1)
         else:
-            print "The execution is successful, Check Output folder for output. All set to use EnsembleMD"
-
+            print(Fore.GREEN+"The execution is successful, Check Output folder for output. All set to use EnsembleMD"+Fore.RESET)
+            
--- test_bb.py
+++ test_bb.py
@@ -1,3 +1,6 @@
+"""
+Sample Code
+"""
 
 from blackbox import Standalone
 
@@ -5,22 +8,41 @@ from blackbox import Standalone
 if __name__ == "__main__":
     test = Standalone(name = 'lsdmap',
                     resource = 'xsede.stampede',
-##                    inp_files = ['-i=/home/suvigya/inp/min.in',
+##                      #amber
+##                    arguments = ['-O',
+##                                 '-i=/home/suvigya/inp/min.in',
 ##                                 '-p=/home/suvigya/inp/penta.top',
 ##                                 '-c=/home/suvigya/inp/penta.crd',
 ##                                 '-inf=/home/suvigya/inp/min.inf',
 ##                                 '-r=/home/suvigya/inp/md.crd',
 ##                                 '-ref=/home/suvigya/inp/min.crd']
-##                      inp_files = ['--topfile=/home/suvigya/inp/penta.top',
-##                                   '--mdfile=/home/suvigya/inp/*.ncdf']
-##                      inp_files = ['runfile=/home/suvigya/inp/run.py',
-##                                   'mdpfile=/home/suvigya/inp/grompp.mdp',
-##                                   'startfile=/home/suvigya/inp/start.gro',
-##                                   'topfile=/home/suvigya/inp/topol.top']
-                      inp_files = ['-f=/home/suvigya/inp/config.ini',
+
+##                      #coco
+##                      arguments = ['--grid=5',
+##                                   '--dims=3',
+##                                   '--frontpoints=4',
+##                                   '--topfile=/home/suvigya/inp/penta.top',
+##                                   '--mdfile=/home/suvigya/inp/*.ncdf',
+##                                   '--mpi','--selection=protein']
+
+##                      #gromacs
+##                      arguments = ['/home/suvigya/inp/run.py',
+##                                   '--mdp=/home/suvigya/inp/grompp.mdp',
+##                                   '--gro=/home/suvigya/inp/start.gro',
+##                                   '--top=/home/suvigya/inp/topol.top']
+
+                      #lsdmap
+                      arguments = ['-f=/home/suvigya/inp/config.ini',
                                    '-w=/home/suvigya/inp/weight.w',
                                    '-c=/home/suvigya/inp/tmp.gro',
                                    '-n=/home/suvigya/inp/out.nn']
+
+                      #amber modules
+##                    modules = ["module load TACC",
+##                           "module load intel/13.0.2.146",
+##                           "module load python/2.7.9",
+##                           "module load netcdf/4.3.2",
+##                           "module load hdf5/1.8.13"]
                     )
     test.run()
     #test.dispFile()
